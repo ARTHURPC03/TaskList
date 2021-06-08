95e10653e092d1e6b57a6f3703fafe06b423a6f9
Arthur Pimentel Cordeiro <55156476+ARTHURPC03@users.noreply.github.com>
2021-06-08 13:33:54 -0300

    🚀finish challenge

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,15 +15,32 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(newTaskTitle === "") {
+      return
+    }
+
+    
+    setTasks([...tasks, {
+      id: Math.floor(Math.random() * 10000 + 1),
+      title: newTaskTitle,
+      isComplete: false,
+    }])
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const tasksChanged = tasks.map(task => {
+      if(task.id === id) {
+        task.isComplete = !task.isComplete
+      }
+      return task
+    })
+  
+    setTasks(tasksChanged)
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    const tasksWithoutRemovedTask = tasks.filter(task => task.id !== id)
+    setTasks(tasksWithoutRemovedTask)
   }
 
   return (
